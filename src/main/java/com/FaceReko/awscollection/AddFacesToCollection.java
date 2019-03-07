6c1ca49a63f8e5d4db6d89916c25e6fc2b6f58c3
karannishad <karan.nishad3@gmail.com>
2019-03-07 15:14:49 +0530

    index faces

---
+++ src/main/java/com/FaceReko/awscollection/AddFacesToCollection.java
@@ -0,0 +1,55 @@
+package com.FaceReko.awscollection;
+
+import com.FaceReko.credentials.Credentials;
+import com.amazonaws.services.rekognition.AmazonRekognition;
+import com.amazonaws.services.rekognition.model.*;
+import com.amazonaws.util.IOUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.util.List;
+
+public class AddFacesToCollection {
+    public static final String collectionId = "MyCollection";
+    public static void main(String[] args) throws Exception {
+        String photo = "/home/consultadd/Downloads/index.jpeg";
+        AmazonRekognition amazonRekognition= Credentials.getClient();
+        ByteBuffer imagebyte;
+        try(InputStream inputStream =new FileInputStream(new File(photo)) ){
+            imagebyte =ByteBuffer.wrap(IOUtils.toByteArray(inputStream));
+        }
+        Image image = new Image()
+               .withBytes(imagebyte);
+
+        IndexFacesRequest indexFacesRequest = new IndexFacesRequest()
+                .withImage(image)
+                .withExternalImageId("try_face")
+                .withMaxFaces(10)
+                .withCollectionId(collectionId)
+                .withDetectionAttributes("DEFAULT");
+
+        IndexFacesResult indexFacesResult = amazonRekognition.indexFaces(indexFacesRequest);
+
+        System.out.println("Results for " + photo);
+        System.out.println("Faces indexed:");
+        List<FaceRecord> faceRecords = indexFacesResult.getFaceRecords();
+        for (FaceRecord faceRecord : faceRecords) {
+            System.out.println("  Face ID: " + faceRecord.getFace().getFaceId());
+            System.out.println("  External Face ID: " + faceRecord.getFace().getExternalImageId());
+
+            System.out.println("  Location:" + faceRecord.getFaceDetail().getBoundingBox().toString());
+        }
+
+        List<UnindexedFace> unindexedFaces = indexFacesResult.getUnindexedFaces();
+        System.out.println("Faces not indexed:");
+        for (UnindexedFace unindexedFace : unindexedFaces) {
+            System.out.println("  Location:" + unindexedFace.getFaceDetail().getBoundingBox().toString());
+            System.out.println("  Reasons:");
+            for (String reason : unindexedFace.getReasons()) {
+                System.out.println("   " + reason);
+            }
+        }
+    }
+}
