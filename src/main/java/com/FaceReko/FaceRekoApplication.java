bbb73852c3a79cd7c92f4e8e07911be1f8c67462
karannishad <karan.nishad3@gmail.com>
2019-03-07 14:23:54 +0530

    initial

---
+++ src/main/java/com/FaceReko/FaceRekoApplication.java
@@ -0,0 +1,13 @@
+package com.FaceReko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceRekoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaceRekoApplication.class, args);
+	}
+
+}
